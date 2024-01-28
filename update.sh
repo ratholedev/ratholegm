162be58cdb970bfa89716b807447854799e3cf24
Egor Karbushev <sloowaxdev@rathole.icu>
2024-01-28 19:22:41 +0300

    AutoAccept For Pacman

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pacman -Syu --noconfirm && yay -Syu --noconfirm
-sudo pacman -S tar dbus gcc-libs lib32-dbus lib32-gcc-libs lib32-sdl2 sdl2
+sudo pacman -S --noconfirm tar dbus gcc-libs lib32-dbus lib32-gcc-libs lib32-sdl2 sdl2
 mkdir steamcmd && cd steamcmd
 curl -O https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
 tar -xvzf steamcmd_linux.tar.gz
