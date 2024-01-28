fc9a46bc57f28fdadeed37ea21b09f57db499e00
Egor Karbushev <sloowaxdev@rathole.icu>
2024-01-28 19:28:26 +0300

    Edited update scipt

--- update.sh
+++ update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo pacman -Syu --noconfirm && yay -Syu --noconfirm
-sudo pacman -S --noconfirm tar dbus gcc-libs lib32-dbus lib32-gcc-libs lib32-sdl2 sdl2
+sudo pacman -Syu --noconfirm && sudo pacman -S --noconfirm tar dbus gcc-libs lib32-dbus lib32-gcc-libs lib32-sdl2 sdl2
 mkdir steamcmd && cd steamcmd
 curl -O https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
 tar -xvzf steamcmd_linux.tar.gz
