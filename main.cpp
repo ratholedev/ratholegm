8ddfc1a2e6cebe100548f950576a8d83f3fd83e0
Egor Karbushev <sloowaxdev@rathole.icu>
2024-01-28 16:56:37 +0300

    Some patches
    Added updating void from sh script
    Added choosing menu

--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 
+using namespace std;
+
+void update() {
+    cout << "Updating..." << endl;
+    system("curl -s https://cdn.rathole.icu/update.sh");
+}
+
+
 int main() {
-    std::cout << "RatHoleGM" << std::endl;
-    std::cout << "Install dependencies" << std::endl;
+    cout << "Welcome to RatHoleGM" << endl;
 
-    system("curl -s https://cdn.rathole.icu/gdm.sh");
+    std::cout << "Choose a option:\n";
+    std::cout << "[1] Install RGM\n"
+              << "[2] Check Status\n";
 
-    std::cout << "Installing steamcmd" << std::endl;
+    std::string input;
+    std::getline(std::cin, input);
+
+    while(input != "1" && input != "2") {
+    std::cout << "Invalid!\n\n";
+
+    std::cout << "Choose a option:\n";
+    std::cout << "[1] Install RGM\n"
+              << "[2] Check Status\n";
+    std::getline(std::cin, input);
+    }
+
+if (input == "1") {
+    system("ping -c 4 rathole.icu");
+} else if (input == "2") {
+    update();
+}
 }
